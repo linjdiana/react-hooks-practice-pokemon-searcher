57015b5cb6cb35326b8eb35e539632278f91248a
Diana Lin <linjdiana@gmail.com>
2023-02-08 21:54:09 -0800

    first two deliverables complete

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "semantic-ui-react";
 
-function PokemonCard() {
+function PokemonCard({ pokemon }) {
+
+  const [isFront, setisFront] = useState(true);
+
+  function handleClick() {
+    setisFront((prevFront => !prevFront))
+  }
   return (
     <Card>
       <div>
-        <div className="image">
-          <img alt="oh no!" />
+        <div className="image" onClick={handleClick}>
+        <img src={isFront ? pokemon.sprites.front : pokemon.sprites.back} alt="oh no!" />
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{pokemon.name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {pokemon.hp}
           </span>
         </div>
       </div>
--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,17 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({ pokemon }) {
+  const pokemonCards = pokemon.map((card) => (
+    <PokemonCard
+    key={card.id}
+    pokemon={card} />
+  ))
+
   return (
     <Card.Group itemsPerRow={6}>
       <h1>Hello From Pokemon Collection</h1>
+      {pokemonCards}
     </Card.Group>
   );
 }
